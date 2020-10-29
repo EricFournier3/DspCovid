54365d9713406dd016be034b558a23c01cbb1f83
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-10-29 11:34:45 -0400

    ef

--- UpdateCovBank.py
+++ UpdateCovBank.py
@@ -27,6 +27,12 @@ class CovBankDB:
         self.ReadConnParam()
         self.connection = self.SetConnection()
 
+        self.patient_col_list = ['PRENOM','NOM','SEXE','DTNAISS','RSS','RTA','NAM']
+        self.prelevement_col_list = ['ID_PATIENT','CODE_HOPITAL','NOM_HOPITAL','ADRESSE_HOPITAL','DATE_PRELEV','GENOME_QUEBEC_REQUETE','DATE_ENVOI_GENOME_QUEBEC','TRAVEL_HISTORY','CT']
+
+    def CloseConnection(self):
+        self.GetConnection().close()
+
     def SetConnection(self):
         return mysql.connector.connect(host=self.host,user=self.user,password=self.password,database=self.database)
 
@@ -40,44 +46,103 @@ class CovBankDB:
         self.connection.commit()
 
     def GetPatientsColumns(self):
-        col_list = ['PRENOM','NOM','SEXE','DTNAISS','RSS','RTA','NAM']
-        return ','.join(col_list)
+        return ','.join(self.patient_col_list)
 
     def Insert(self):
         logging.info("Begin insert")
 
+        self.nb_patients_inserted = 0
+
         for index, row in self.envois_genome_qc_obj.pd_df.loc[:,].iterrows():
-            #print(row)
             nom = row['Nom']
             prenom = row['Prénom']
             date_naiss = row['Date de naissance']
             date_prelev = row['Date de prélèvement']
             nam = row['NAM']
-            #print("************************")
             tsp_geo_match_df = self.GetTspGeoMatch(nom,prenom,date_naiss,nam,date_prelev)
-            #print(tsp_geo_match_df)
 
             if tsp_geo_match_df.shape[0] != 0:
-                patient_record = self.GetPatientValToInsert(tsp_geo_match_df,row,'HOSPITAL_NAME','HOSPITAL_ADDRESS' )
-                self.InsertPatient(patient_record)
+                patient_record = self.GetPatientValToInsert(tsp_geo_match_df)
+                patient_id = self.InsertPatient(patient_record)
+                if patient_id is not None:
+                    prelevement_record = self.GetPrelevementToInsert(tsp_geo_match_df,row,patient_id,"NOM_HOPITAL","ADDRESS_HOPITAL")
+                    print(prelevement_record)
+                else:
+                    logging.error("Impossible d inserer ce prelevement " + str(row))
 
+    def CheckIfPatientExist(self,patient_record,cursor):
+        rec = dict(list(zip(self.patient_col_list,patient_record)))
+
+        sql = "SELECT ID_PATIENT from Patients where PRENOM = '{0}' and NOM = '{1}' and SEXE = '{2}' and DTNAISS = '{3}' and RSS = '{4}' and RTA = '{5}' and NAM = '{6}'".format(rec['PRENOM'],rec['NOM'],rec['SEXE'],rec['DTNAISS'],rec['RSS'],rec['RTA'],rec['NAM'])
+
+        cursor.execute(sql)
+        id_patient_tuple_list = cursor.fetchall()
+        nb_patient = len(id_patient_tuple_list)
+
+        if nb_patient < 1:
+            return [False,None]
+        elif nb_patient == 1:
+            id_patient = id_patient_tuple_list[0][0] 
+            return [True,id_patient]
+        elif nb_patient > 1:
+            logging.error("Probleme : plus de un seul match Patients " + str(id_patient_tuple_list))
+            return [True,None]
+        
     def InsertPatient(self,patient_record):
-        #self.CheckIfPatientExist()
+        cursor = self.GetCursor()
+        exist_list = self.CheckIfPatientExist(patient_record,cursor)
+        exist = exist_list[0]
+        patient_id = exist_list[1]
 
-        try:
-            ncols = len(patient_record)
-            print(self.GetPatientsColumns())
+        if not exist:
+ 
+            try:
+                ncols = len(patient_record)
+                sql_insert = "INSERT INTO Patients ({0}) values ({1})".format(self.GetPatientsColumns(),str("%s,"*ncols)[:-1])
+                cursor.execute(sql_insert,patient_record)
+                cursor.execute("SELECT LAST_INSERT_ID()")
+                patient_id = cursor.fetchone()[0]
+                
+                cursor.close()
+                self.Commit()
+                self.nb_patients_inserted += 1
+                sys.stdout.write("Insert in Patient >>> %d\r"%self.nb_patients_inserted)
+                sys.stdout.flush()
+                return patient_id
+            except mysql.connector.Error as err:
+                logging.error("Erreur d'insertion dans la table Patients avec le record " + str(patient_record))
+                print(err)
+                return None
+        elif exist and (patient_id is not None):
+            return patient_id
+        else:
+            return None      
 
-        except:
-            pass
+    def GetPrelevementToInsert(self,tsp_geo_match_df,envois_genome_qc,patient_id,nom_hopital,adresse_hopital):
+        #self.prelevement_col_list = ['ID_PATIENT','CODE_HOPITAL','NOM_HOPITAL','ADRESSE_HOPITAL','DATE_PRELEV','GENOME_QUEBEC_REQUETE','DATE_ENVOI_GENOME_QUEBEC','TRAVEL_HISTORY','CT']
 
-
-    def GetPatientValToInsert(self,tsp_geo_match_df,envois_genome_qc_row,hospital_name,hospital_address):
         def GetVal(x):
             return x
 
-        nom = tsp_geo_match_df['nom'].values[0]
+        patient_id = patient_id
+        code_hopital = envois_genome_qc['Hopital']
+        nom_hopital = nom_hopital
+        adresse_hopital = adresse_hopital
+        date_prelev = tsp_geo_match_df['date_prel'].values[0]
+        genome_quebec_requete = envois_genome_qc['# Requête']
+        date_envois_genome_quebec = envois_genome_qc['DateEnvoiGenomeQuebec']
+        travel_history = 'TRAVEL_HISTORY' # prendre de tsp_geo mais temporairement de sgil
+        ct = 'CT' # prendre de tsp_geo mais temporaire de sgil
+
+        return(tuple(map(GetVal,(patient_id,code_hopital,nom_hopital,adresse_hopital,date_prelev,genome_quebec_requete,date_envois_genome_quebec,travel_history,ct))))
+        
+        
+    def GetPatientValToInsert(self,tsp_geo_match_df):
+        def GetVal(x):
+            return x
+
         prenom = tsp_geo_match_df['prenom'].values[0]
+        nom = tsp_geo_match_df['nom'].values[0]
 
         sexe =  tsp_geo_match_df['sexe'].values[0]
         if str(sexe) == "1":
@@ -88,10 +153,14 @@ class CovBankDB:
             sexe = "Inconnu" 
 
         date_naiss = tsp_geo_match_df['date_nais'].values[0]        
+        #print(tsp_geo_match_df['date_nais'].dtype, " --  ", type(date_naiss))
+        date_naiss = str(date_naiss)
         rss = tsp_geo_match_df['RSS'].values[0]
         rta = tsp_geo_match_df['RTA'].values[0]
+        nam = tsp_geo_match_df['nam'].values[0]
+        nam = str(nam)
 
-        return(tuple(map(GetVal,(nom,prenom,sexe,date_naiss,rss,rta,hospital_name,hospital_address))))
+        return(tuple(map(GetVal,(prenom,nom,sexe,date_naiss,rss,rta,nam))))
 
     def GetTspGeoMatch(self,nom,prenom,date_naiss,nam,date_prelev):
         match_df = pd.DataFrame()
@@ -200,6 +269,7 @@ def Main():
     cov_bank_db = CovBankDB(tsp_geo_obj,envois_genome_qc_obj)
 
     cov_bank_db.Insert()
+    cov_bank_db.CloseConnection()
 
 if __name__ == '__main__':
     Main()
